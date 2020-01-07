547ec18f55f3e7043b99f0e41ee0adc5f2c07887
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2020-01-06 21:43:54 -0500

    Add files via upload

---
+++ examples/ESP_FSWebServer/ESP_FSWebServer.ino
@@ -0,0 +1,376 @@
+/****************************************************************************************************************************
+ * ESP_FSWebServer - Example WebServer with SPIFFS backend for esp8266
+ * For ESP8266 boards
+ *
+ * ESP_WiFiManager is a library for the ESP8266/ESP32 platform (https://github.com/esp8266/Arduino) to enable easy
+ * configuration and reconfiguration of WiFi credentials using a Captive Portal.
+ *
+ * Forked from Tzapu https://github.com/tzapu/WiFiManager
+ * and from Ken Taylor https://github.com/kentaylor
+ * 
+ * Built by Khoi Hoang https://github.com/khoih-prog/ESP_WiFiManager
+ * Licensed under MIT license
+ * Version: 1.0.3
+ * 
+ * Example modified from https://github.com/esp8266/Arduino/blob/master/libraries/ESP8266WebServer/examples/FSBrowser/FSBrowser.ino
+ * 
+ * Copyright (c) 2015 Hristo Gochkov. All rights reserved.
+ * This file is part of the ESP8266WebServer library for Arduino environment.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ * Version Modified By   Date      Comments
+ * ------- -----------  ---------- --------------------------------------------
+ *  1.0.3   K Hoang      06/01/2020 Add code to use ESP_WiFiManager
+ *****************************************************************************************************************************/
+/*****************************************************************************************************************************
+ * How To Use: 
+ * 1) Upload the contents of the data folder with MkSPIFFS Tool ("ESP8266 Sketch Data Upload" in Tools menu in Arduino IDE)
+ * 2) or you can upload the contents of a folder if you CD in that folder and run the following command:
+ *    for file in `\ls -A1`; do curl -F "file=@$PWD/$file" esp8266fs.local/edit; done
+ * 3) access the sample web page at http://esp8266fs.local
+ * 4) edit the page by going to http://esp8266fs.local/edit
+*****************************************************************************************************************************/
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <FS.h>
+
+#define USE_SPIFFS      true
+
+#if USE_SPIFFS
+  FS* filesystem = &SPIFFS;
+#else
+  #include <LittleFS.h>
+  FS* filesystem = &LittleFS;
+#endif
+
+#define DBG_OUTPUT_PORT Serial
+
+#define ESP_getChipId()   (ESP.getChipId())
+
+// SSID and PW for Config Portal
+String ssid = "ESP_" + String(ESP_getChipId(), HEX);
+const char* password = "your_password";
+
+// SSID and PW for your Router
+String Router_SSID;
+String Router_Pass;
+
+// Use false if you don't like to display Available Pages in Information Page of Config Portal
+// Comment out or use true to display Available Pages in Information Page of Config Portal
+// Must be placed before #include <ESP_WiFiManager.h> 
+#define USE_AVAILABLE_PAGES     false
+
+#include <ESP_WiFiManager.h>              //https://github.com/khoih-prog/ESP_WiFiManager
+
+const char* host = "esp8266fs";
+
+ESP8266WebServer server(80);
+
+//holds the current upload
+File fsUploadFile;
+
+//format bytes
+String formatBytes(size_t bytes) {
+  if (bytes < 1024) {
+    return String(bytes) + "B";
+  } else if (bytes < (1024 * 1024)) {
+    return String(bytes / 1024.0) + "KB";
+  } else if (bytes < (1024 * 1024 * 1024)) {
+    return String(bytes / 1024.0 / 1024.0) + "MB";
+  } else {
+    return String(bytes / 1024.0 / 1024.0 / 1024.0) + "GB";
+  }
+}
+
+String getContentType(String filename) {
+  if (server.hasArg("download")) {
+    return "application/octet-stream";
+  } else if (filename.endsWith(".htm")) {
+    return "text/html";
+  } else if (filename.endsWith(".html")) {
+    return "text/html";
+  } else if (filename.endsWith(".css")) {
+    return "text/css";
+  } else if (filename.endsWith(".js")) {
+    return "application/javascript";
+  } else if (filename.endsWith(".png")) {
+    return "image/png";
+  } else if (filename.endsWith(".gif")) {
+    return "image/gif";
+  } else if (filename.endsWith(".jpg")) {
+    return "image/jpeg";
+  } else if (filename.endsWith(".ico")) {
+    return "image/x-icon";
+  } else if (filename.endsWith(".xml")) {
+    return "text/xml";
+  } else if (filename.endsWith(".pdf")) {
+    return "application/x-pdf";
+  } else if (filename.endsWith(".zip")) {
+    return "application/x-zip";
+  } else if (filename.endsWith(".gz")) {
+    return "application/x-gzip";
+  }
+  return "text/plain";
+}
+
+bool handleFileRead(String path) {
+  DBG_OUTPUT_PORT.println("handleFileRead: " + path);
+  if (path.endsWith("/")) {
+    path += "index.htm";
+  }
+  String contentType = getContentType(path);
+  String pathWithGz = path + ".gz";
+  if (filesystem->exists(pathWithGz) || filesystem->exists(path)) {
+    if (filesystem->exists(pathWithGz)) {
+      path += ".gz";
+    }
+    File file = filesystem->open(path, "r");
+    server.streamFile(file, contentType);
+    file.close();
+    return true;
+  }
+  return false;
+}
+
+void handleFileUpload() {
+  if (server.uri() != "/edit") {
+    return;
+  }
+  HTTPUpload& upload = server.upload();
+  if (upload.status == UPLOAD_FILE_START) {
+    String filename = upload.filename;
+    if (!filename.startsWith("/")) {
+      filename = "/" + filename;
+    }
+    DBG_OUTPUT_PORT.print("handleFileUpload Name: "); DBG_OUTPUT_PORT.println(filename);
+    fsUploadFile = filesystem->open(filename, "w");
+    filename.clear();
+  } else if (upload.status == UPLOAD_FILE_WRITE) {
+    //DBG_OUTPUT_PORT.print("handleFileUpload Data: "); DBG_OUTPUT_PORT.println(upload.currentSize);
+    if (fsUploadFile) {
+      fsUploadFile.write(upload.buf, upload.currentSize);
+    }
+  } else if (upload.status == UPLOAD_FILE_END) {
+    if (fsUploadFile) {
+      fsUploadFile.close();
+    }
+    DBG_OUTPUT_PORT.print("handleFileUpload Size: "); DBG_OUTPUT_PORT.println(upload.totalSize);
+  }
+}
+
+void handleFileDelete() {
+  if (server.args() == 0) {
+    return server.send(500, "text/plain", "BAD ARGS");
+  }
+  String path = server.arg(0);
+  DBG_OUTPUT_PORT.println("handleFileDelete: " + path);
+  if (path == "/") {
+    return server.send(500, "text/plain", "BAD PATH");
+  }
+  if (!filesystem->exists(path)) {
+    return server.send(404, "text/plain", "FileNotFound");
+  }
+  filesystem->remove(path);
+  server.send(200, "text/plain", "");
+  path.clear();
+}
+
+void handleFileCreate() {
+  if (server.args() == 0) {
+    return server.send(500, "text/plain", "BAD ARGS");
+  }
+  String path = server.arg(0);
+  DBG_OUTPUT_PORT.println("handleFileCreate: " + path);
+  if (path == "/") {
+    return server.send(500, "text/plain", "BAD PATH");
+  }
+  if (filesystem->exists(path)) {
+    return server.send(500, "text/plain", "FILE EXISTS");
+  }
+  File file = filesystem->open(path, "w");
+  if (file) {
+    file.close();
+  } else {
+    return server.send(500, "text/plain", "CREATE FAILED");
+  }
+  server.send(200, "text/plain", "");
+  path.clear();
+}
+
+void handleFileList() {
+  if (!server.hasArg("dir")) {
+    server.send(500, "text/plain", "BAD ARGS");
+    return;
+  }
+
+  String path = server.arg("dir");
+  DBG_OUTPUT_PORT.println("handleFileList: " + path);
+  Dir dir = filesystem->openDir(path);
+  path.clear();
+
+  String output = "[";
+  while (dir.next()) {
+    File entry = dir.openFile("r");
+    if (output != "[") {
+      output += ',';
+    }
+    bool isDir = false;
+    output += "{\"type\":\"";
+    output += (isDir) ? "dir" : "file";
+    output += "\",\"name\":\"";
+    if (entry.name()[0] == '/') {
+      output += &(entry.name()[1]);
+    } else {
+      output += entry.name();
+    }
+    output += "\"}";
+    entry.close();
+  }
+
+  output += "]";
+  server.send(200, "text/json", output);
+}
+
+void setup(void) {
+  DBG_OUTPUT_PORT.begin(115200);
+  DBG_OUTPUT_PORT.print("\n");
+  DBG_OUTPUT_PORT.setDebugOutput(true);
+  filesystem->begin();
+  {
+    // Uncomment to format FS. Remember to uncomment after done
+    //filesystem->format();
+    Dir dir = filesystem->openDir("/");
+    DBG_OUTPUT_PORT.println("Opening / directory");
+    while (dir.next()) {
+      String fileName = dir.fileName();
+      size_t fileSize = dir.fileSize();
+      DBG_OUTPUT_PORT.printf("FS File: %s, size: %s\n", fileName.c_str(), formatBytes(fileSize).c_str());
+    }
+    DBG_OUTPUT_PORT.printf("\n");
+  }
+
+  unsigned long startedAt = millis();
+  
+ //Local intialization. Once its business is done, there is no need to keep it around
+  ESP_WiFiManager ESP_wifiManager;
+  WiFi.hostname("ESP_FSWebServer");
+
+  ESP_wifiManager.setMinimumSignalQuality(-1);
+
+  // We can't use WiFi.SSID() in ESP32as it's only valid after connected. 
+  // SSID and Password stored in ESP32 wifi_ap_record_t and wifi_config_t are also cleared in reboot
+  // Have to create a new function to store in EEPROM/SPIFFS for this purpose
+  Router_SSID = ESP_wifiManager.WiFi_SSID();
+  Router_Pass = ESP_wifiManager.WiFi_Pass();
+  
+  //Remove this line if you do not want to see WiFi password printed
+  DBG_OUTPUT_PORT.println("Stored: SSID = " + Router_SSID + ", Pass = " + Router_Pass);
+
+  // SSID to uppercase 
+  ssid.toUpperCase();
+  
+  if (Router_SSID == "")
+  {
+    DBG_OUTPUT_PORT.println("We haven't got any access point credentials, so get them now");   
+      
+    // Starts an access point
+    if (!ESP_wifiManager.startConfigPortal((const char *) ssid.c_str(), password)) 
+      DBG_OUTPUT_PORT.println("Not connected to WiFi but continuing anyway.");
+    else 
+      DBG_OUTPUT_PORT.println("WiFi connected...yeey :)");    
+  }
+
+  #define WIFI_CONNECT_TIMEOUT        30000L
+  #define WHILE_LOOP_DELAY            200L
+  #define WHILE_LOOP_STEPS            (WIFI_CONNECT_TIMEOUT / ( 3 * WHILE_LOOP_DELAY ))
+  
+  startedAt = millis();
+  
+  while ( (WiFi.status() != WL_CONNECTED) && (millis() - startedAt < WIFI_CONNECT_TIMEOUT ) )
+  {   
+    WiFi.mode(WIFI_STA);
+    WiFi.persistent (true);
+
+    // We start by connecting to a WiFi network 
+    DBG_OUTPUT_PORT.print("Connecting to ");
+    DBG_OUTPUT_PORT.println(Router_SSID);
+  
+    WiFi.begin(Router_SSID.c_str(), Router_Pass.c_str());
+
+    int i = 0;
+    while((!WiFi.status() || WiFi.status() >= WL_DISCONNECTED) && i++ < WHILE_LOOP_STEPS)
+    {
+      delay(WHILE_LOOP_DELAY);
+    }    
+  }
+  
+  DBG_OUTPUT_PORT.println("");
+  DBG_OUTPUT_PORT.print("Connected! IP address: ");
+  DBG_OUTPUT_PORT.println(WiFi.localIP());
+
+  MDNS.begin(host);
+  DBG_OUTPUT_PORT.print("Open http://");
+  DBG_OUTPUT_PORT.print(host);
+  DBG_OUTPUT_PORT.println(".local/edit to see the file browser");
+
+
+  //SERVER INIT
+  //list directory
+  server.on("/list", HTTP_GET, handleFileList);
+  //load editor
+  server.on("/edit", HTTP_GET, []() {
+    if (!handleFileRead("/edit.htm")) {
+      server.send(404, "text/plain", "FileNotFound");
+    }
+  });
+  //create file
+  server.on("/edit", HTTP_PUT, handleFileCreate);
+  //delete file
+  server.on("/edit", HTTP_DELETE, handleFileDelete);
+  //first callback is called after the request has ended with all parsed arguments
+  //second callback handles file uploads at that location
+  server.on("/edit", HTTP_POST, []() {
+    server.send(200, "text/plain", "");
+  }, handleFileUpload);
+
+  //called when the url is not defined here
+  //use it to load content from SPIFFS
+  server.onNotFound([]() {
+    if (!handleFileRead(server.uri())) {
+      server.send(404, "text/plain", "FileNotFound");
+    }
+  });
+
+  //get heap status, analog input value and all GPIO statuses in one json call
+  server.on("/all", HTTP_GET, []() {
+    String json('{');
+    json += "\"heap\":" + String(ESP.getFreeHeap());
+    json += ", \"analog\":" + String(analogRead(A0));
+    json += ", \"gpio\":" + String((uint32_t)(((GPI | GPO) & 0xFFFF) | ((GP16I & 0x01) << 16)));
+    json += "}";
+    server.send(200, "text/json", json);
+    json.clear();
+  });
+  server.begin();
+  DBG_OUTPUT_PORT.println("HTTP server started");
+
+}
+
+void loop(void) {
+  server.handleClient();
+  MDNS.update();
+}
